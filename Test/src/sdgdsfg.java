3294bae04844e175b1fa4f07c9393ef05be62a70
jedy-suraj <surajshindejedy@gmail.com>
2024-10-17 04:28:46 +0530

    number printed

--- Test/src/sdgdsfg.java
+++ Test/src/sdgdsfg.java
@@ -4,6 +4,9 @@ public class sdgdsfg {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Suraj");
+		for(int i=0; i<=10; i++) {
+			System.out.println(i);
+		}
 
 	}
 
